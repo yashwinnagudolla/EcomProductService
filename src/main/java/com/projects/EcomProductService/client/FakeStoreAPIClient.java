b384d84e1037f3110eb6534f1c83fa65e11ae876
Yashwin Nagudolla <yashwinnagudolla@gmail.com>
2024-01-23 21:23:39 -0500

    Added method defenitions for the FakeStoreAPIClient

--- src/main/java/com/projects/EcomProductService/client/FakeStoreAPIClient.java
+++ src/main/java/com/projects/EcomProductService/client/FakeStoreAPIClient.java
@@ -1,7 +1,28 @@
 package com.projects.EcomProductService.client;
 
+import com.projects.EcomProductService.dto.FakeStoreProductRequestDTO;
+import com.projects.EcomProductService.dto.FakeStoreProductResponseDTO;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 
+import java.util.List;
+
 public class FakeStoreAPIClient {
-    private RestTemplateBuilder
+    private RestTemplateBuilder restTemplateBuilder;
+
+    public List<FakeStoreProductResponseDTO> getAllProducts(){
+        return null;
+    }
+
+    public FakeStoreProductResponseDTO getProductById(int id){
+        return null;
+    }
+
+    public FakeStoreProductResponseDTO createProduct(FakeStoreProductRequestDTO fakeStoreProductRequestDTO){
+        return null;
+    }
+
+    public FakeStoreProductResponseDTO deleteProduct(int id){
+        return null;
+    }
+
 }
