5b5b720d399cc2b89d597896d1c51a279a586ed3
Yashwin Nagudolla <yashwinnagudolla@gmail.com>
2024-01-24 21:41:13 -0500

    Application properties added

--- src/main/java/com/projects/EcomProductService/client/FakeStoreAPIClient.java
+++ src/main/java/com/projects/EcomProductService/client/FakeStoreAPIClient.java
@@ -3,11 +3,18 @@ package com.projects.EcomProductService.client;
 import com.projects.EcomProductService.dto.FakeStoreProductRequestDTO;
 import com.projects.EcomProductService.dto.FakeStoreProductResponseDTO;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Component
 public class FakeStoreAPIClient {
     private RestTemplateBuilder restTemplateBuilder;
+
+    public FakeStoreAPIClient(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplateBuilder = restTemplateBuilder;
+    }
 
     public List<FakeStoreProductResponseDTO> getAllProducts(){
         return null;
